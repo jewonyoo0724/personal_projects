743bef8c456e3d9fd5b052450a33104479552f51
Jewon Yoo <skywalker0724@gmail.com>
2025-09-02 15:21:22 -0700

    non-func

--- groupCalc.c
+++ groupCalc.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-typedef struct{
-
-};
-
+char *getCycle(char**cycle, int index, int card);
 char *groupCalc(char *cycle);
 
 int main (int argc, char **argv){
 
     int numCycle;
     int card;
+    int maxLength;
     char *comp = malloc(sizeof(char) * numCycle);
     char **cycles;
 
     //Sn -> get n
     printf("Cardinality of symmetric group: ");
     scanf("%i", &card);
+    printf("\n");
+
+    maxLength = 3 * card;
 
     //num of cycles in Sn
     printf("Enter number of cycles: ");
     scanf("%i", &numCycle);
+    printf("\n");
 
     //array of strings. size numCycle. strings cannot be more than n (excluding parantheses)
     cycles = malloc(numCycle * sizeof);
 
-    for (int i = 0; i < numCycle; i++)
-    {
-        printf("Cycle %i: ", i+1);
-        fgets(cycles[i], sizeof(cycles[i]), stdin);
+    //get cycles
+    for (int i = 0; i < numCycle; i++){
+        getCycles(i);
     }
-
+    getCycles
     printf("Enter composition order: ");
     scanf("%s", comp);
 
@@ -39,8 +40,22 @@ int main (int argc, char **argv){
     {
         free(cycle[i]);
     }
+
     free(comp);
     free(cycles);
+}
+
+char *getCycle(char**cycle, int index, int card, int maxLength){
+
+    printf("Cycle %i (omit if trivial): ", index+1);
+    cycle[index] = malloc(maxLength * sizeof(char));
+
+    fgets(cycles[index], maxLength * sizeof(char), stdin); //(1)...(n) max
+    if (cycles[index] == NULL)
+    {
+
+    }
+    cycles[index] = malloc(sizeof();
 }
 
 char *groupCalc(char *cycle){
